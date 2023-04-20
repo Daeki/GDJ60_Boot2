31134c7032fc195057772f096712f97938e5eac1
Daeki <kimdaeki@gmail.com>
2023-04-20 09:15:10 +0900

    board1

---
+++ src/test/java/com/iu/base/board/notice/NoticeDAOTest.java
@@ -0,0 +1,37 @@
+package com.iu.base.board.notice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.iu.base.board.BoardVO;
+
+@SpringBootTest
+class NoticeDAOTest {
+
+	@Autowired
+	private NoticeDAO noticeDAO;
+	
+	//@Test
+	void setInsertTest()throws Exception{
+		for(int i=0;i<120;i++) {
+			BoardVO boardVO = new NoticeVO();
+			
+			boardVO.setWriter("iu"+i);
+			boardVO.setTitle("Title Test"+i);
+			boardVO.setContents("Contents Test"+i);
+			
+			int result = noticeDAO.setInsert(boardVO);
+			
+			if(i%10==0) {
+				Thread.sleep(500);
+			}
+		
+		}
+		System.out.println("종료");
+		
+	}
+
+}
